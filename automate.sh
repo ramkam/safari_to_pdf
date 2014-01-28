2116e3f71151031fd67a351091cdfc92e19f8c62
ramkam <contact@ramkam.com>
2014-01-28 22:12:45 +0100

    minor

--- automate.sh
+++ automate.sh
@@ -6,6 +6,11 @@
 # Last time we used the script, it was pointing there
 pdfdir=/Users/RK/Dropbox/Safari\ To\ Pdf 
 
+# Provisionning for a user space that will not be uploaded to git
+# usefull for url lists ... for example ...
+mkdir -p userdata
+
+
 echo "The url ?"
 read url
 if [ ! -z "$url" ]; then
